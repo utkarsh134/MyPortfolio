abaacca5bf92cb6cddda7d2d06e6d7c2aed366b1
utkarsh134 <utkarshpatidar450@gmail.com>
2024-02-29 12:36:52 +0530

    LazyLoad images is done

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-let sideMenu = document.getElementById('sideMenu') ;
+let sideMenu = document.getElementById("sideMenu");
 
-function openMenu(){
-    sideMenu.style.right = "0" ;
-    
+function openMenu() {
+  sideMenu.style.right = "0";
 }
-function closeMenu(){
-    sideMenu.style.right = "-200px" ;
+function closeMenu() {
+  sideMenu.style.right = "-200px";
 }
+
+const blurDivs = document.querySelectorAll(".blur-load");
+blurDivs.forEach((div) => {
+  const img = div.querySelector("img");
+
+  function loaded() {
+    // show img
+    div.classList.add("loaded");
+  }
+
+  if (img.complete) {
+    loaded();
+  } else {
+    img.addEventListener("load", loaded);
+  }
+});
